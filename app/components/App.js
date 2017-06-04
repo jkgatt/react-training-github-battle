e0f9e08297f300d58261d9d41222c2def429d839
Jean Gatt <jeangatt@gmail.com>
2017-06-04 18:47:28 +0200

    Added the routing and two other pages

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router,
+            Route,
+            Switch
+        }
+        from 'react-router-dom';
+
 
 import {Popular} from './Popular';
+import Home from './Home';
+import Battle from './Battle';
+import Nav from './Nav';
 
 export class App extends React.Component {
 	render() {
 		return (
+            <Router>
 				<div className='container'>
-					<Popular />
+                    <Nav />
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route path='/battle' component={Battle} />
+    					<Route path='/popular' component={Popular} />
+                        <Route render={() => {
+                            return <p>Not Found</p>
+                        }} />
+                    </Switch>
 				</div>
-			);
+            </Router>
+		);
 	};
 }
