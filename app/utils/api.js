38a2f7869e0f76ad695e2132f9238fc59efdef4b
Jean Gatt <jeangatt@gmail.com>
2017-06-04 22:04:09 +0200

    Completed the api class

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,5 +1,57 @@
 import axios from 'axios';
 
+var params = "";
+
+function getProfile(username) {
+    return axios.get('https://api.github.com/users/' + username + params)
+        .then((user) => {
+            return user.data;
+        });
+}
+
+function getRepos(username){
+    return axios.get('https://api.github.com/users/' + username + '/repos'+ params
+            + '&per_page=100');
+}
+
+function getStarCount(repos) {
+    return repos.data.reduce((count, repo) => {
+        return count + repo.stargazers_count;
+    }, 0);
+}
+function calculateScore(profile, repos) {
+    var followers = profile.followers;
+    var totalStars = getStarCount(repos);
+
+    return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then((data) => {
+        var profile = data[0];
+        var repos = data[1];
+
+        return {
+            profile: profile,
+            score: calculateScore(profile, repos)
+        }
+    })
+}
+
+function sortPlayers(player) {
+    return players.sort((a,b) => {
+        return b.score - a.score;
+    });
+}
+
 module.exports = {
     fetchPopularRepos: (language) => {
         var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+
@@ -8,5 +60,10 @@ module.exports = {
         return axios.get(encodedURI). then((response) => {
                 return response.data.items;
         })
+    },
+    battle: (players) => {
+        return axios.all(
+            players.map(getUserData)
+        )
     }
 }
